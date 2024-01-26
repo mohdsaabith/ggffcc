d716123aaddc5222abb2532a6cc41ef44139e3a0
Mohammed Saabith Cp <44156514+mohdsaabith@users.noreply.github.com>
2024-01-26 19:47:18 +0400

    3rd  Commits

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,34 +1,41 @@
 import React from "react";
+import { useRouter } from "next/router";
 
-export default function index() {
+export default function Layout() {
+  const router = useRouter();
   const menuList = ["Home", "Pricing", "Docs", "Contact"];
 
   return (
     <div className="w-auto">
-      <div className="w-[100px] bg-slate-500">
-        <div className=" flex flex-col justify-between items-center mx-8">
+      <div className="w-[100px] bg-slate-500 border-[2px] border-black">
+        <div className=" flex flex-col justify-between items-center mx-8 ">
           <div className=" w-[100px]">
-            <div className="h-[10px] bg-blue-700"></div>
+            <div className="h-[10px] bg-blue-700 border-b-[2px] border-x-[2px] border-black"></div>
             <div className="text-lg px-[10px] flex items-center">Home</div>
             {/* <div className="h-[10px] bg-blue-700"></div> */}
           </div>
 
           <div className=" w-[100px]">
-            <div className="h-[10px] bg-blue-700"></div>
-            <div className="text-lg px-[10px] flex items-center">Pricing</div>
+            <div className="h-[10px] bg-blue-700 border-[2px] border-black"></div>
+            <div
+              className="text-lg px-[10px] flex items-center cursor-pointer"
+              onClick={() => router.push("/Pricing")}
+            >
+              Pricing
+            </div>
             {/* <div className="h-[10px] bg-blue-700"></div> */}
           </div>
 
           <div className=" w-[100px]">
-            <div className="h-[10px] bg-blue-700"></div>
+            <div className="h-[10px] bg-blue-700 border-[2px] border-black"></div>
             <div className="text-lg px-[10px] flex items-center">Docs</div>
             {/* <div className="h-[10px] bg-blue-700"></div> */}
           </div>
 
           <div className=" w-[100px]">
-            <div className="h-[10px] bg-blue-700"></div>
+            <div className="h-[10px] bg-blue-700 border-[2px] border-black"></div>
             <div className="text-lg px-[10px] flex items-center">Contact</div>
-            <div className="h-[10px] bg-blue-700"></div>
+            <div className="h-[10px] bg-blue-700 border-t-[2px] border-x-[2px] border-black"></div>
           </div>
         </div>
       </div>
